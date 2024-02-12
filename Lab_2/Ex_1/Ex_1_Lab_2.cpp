522dcd0c5919b8b4a3708dfa9845fa9b7cf79523
VlasMaru <118015689+VlasMaru@users.noreply.github.com>
2024-02-12 21:23:20 +0200

    -

---
+++ Lab_2/Ex_1/Ex_1_Lab_2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::vector <char> letters{ 'К','О','М','Б','И','Н','А','Т','О','Р','И','К','А' };
+    std::set <std::string> words;
+    for (int x = 0; x < 12; x++)
+    {
+        for (int y = 0; y < 13; y++)
+        {
+            for (int k = 0; k < 13; k++)
+            {
+                for (int z = 0; z < 13; z++)
+                {
+                    std::set <int> ind;
+                    ind.insert(x);
+                    ind.insert(y);
+                    ind.insert(z);
+                    ind.insert(k);
+                    if (ind.size() == 4)
+                    {
+                        std::string word_o = "";
+                        word_o += letters[x];
+                        word_o += letters[y];
+                        word_o += letters[k];
+                        word_o += letters[z];
+                        words.insert(word_o);
+                        word_o = "";
+                    }
+
+                    ind.clear();
+                }
+            }
+        }
+    }
+
+    std::cout << words.size();
+
+}
